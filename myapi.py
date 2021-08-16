322ab3703752876405ea22d3ee6f4a6bb6805f73
alanb-90 <alanbelek90@gmail.com>
2021-08-16 14:56:40 +0200

    update

--- myapi.py
+++ myapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,9 +8,19 @@ students = {
     1: {
         'name': 'john',
         'age': 17,
-        'class': 'year 12'
+        'year': 'year 12'
     }
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+    
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
 
 @app.get("/")
 def index():
@@ -28,3 +39,28 @@ def get_student(*, student_id: int, name: Optional[str] = None, test: int):
         if students[student_id]['name'] == name:
             return students[student_id]
     return{'Data': 'Not found'}
+
+@app.post('/create-student/{student_id}')
+def create_student(student_id : int, student : Student):
+    if student_id in students:
+        return {'Error': 'Student exists'}
+    
+    students[student_id] = student
+    return students[student_id]
+
+
+@app.put('/update-student/{student_id}')
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {'Error': 'Student does not exsist'}
+    
+    if student.name != None: 
+        student[student_id].name = student.name
+        
+    if student.age != None:
+        student[student_id].age = student.age
+        
+    if student.year != None:
+        student[student_id].year = student.year
+    
+    return students[student_id]
