6af2c4ef9fa577051ca5ab4a0da418d5a97ea10a
alanb-90 <alanbelek90@gmail.com>
2021-08-16 14:26:36 +0200

    third

--- myapi.py
+++ myapi.py
@@ -1,10 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
+
+students = {
+    1: {
+        'name': 'john',
+        'age': 17,
+        'class': 'year 12'
+    }
+}
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
 
 
-def this:
+@app.get("/get-student/{student_id}")
+# access the Json format of the student with the number from key:value dictionary: description path states what it needs 
+def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt =0 , lt = 3)):
+    return students[student_id]
+
+
+@app.get('/get-by-name/{student_id}')
+def get_student(*, student_id: int, name: Optional[str] = None, test: int):
+    for student_id in students:
+        if students[student_id]['name'] == name:
+            return students[student_id]
+    return{'Data': 'Not found'}
